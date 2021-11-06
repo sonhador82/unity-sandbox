8055b3c6fd3d7a96b7f79ae93381d6a9220c4c04
Дмитрий Шелестовский <dashelesto@mts.ru>
2021-11-06 18:39:36 +0300

    init

---
+++ ObstacleCourse/Assets/Scripts/Hazard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision projectile");
+        if (other.gameObject.layer == 8)
+        {
+                Player player = other.GetComponent<Player>();
+        
+                if (player != null)
+                player.Die();
+        }
+    }
+}
